85c8259350d72a827607f62f5697931231e5786f
Olsen <chrisolsen1993@gmail.com>
2022-02-09 21:03:52 -0500

    oui

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,7 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Message, Conversation } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
+//here we go
 const resolvers = {
   Query: {
     categories: async () => {
