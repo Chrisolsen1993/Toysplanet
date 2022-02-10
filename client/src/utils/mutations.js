50ba307572c502564e1b2d64d488bb92c189b06a
Olsen <chrisolsen1993@gmail.com>
2022-02-09 20:45:13 -0500

    ok

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -49,3 +49,35 @@ export const ADD_USER = gql`
     }
   }
 `;
+// export const CREATE_CONVERSATION = gql`
+// createConversations ($id:ID!){
+// addConversation(
+//      id:$id
+// )   
+//   {
+//      members{
+//        _id
+     
+//       }
+//     _id
+//   }
+// }
+
+// `
+// export const SEND_MESSAGE = gql`
+// sendMessage($conversationId:String!, $senderId:ID!, $text:String!){
+//   createMessage(
+//     conversationId:$conversationId,
+//     senderId:$senderId,
+//     text:$text
+//   ){
+//     conversationId
+//      sender{
+//        _id
+//        }
+//      text
+  
+//      }
+//   }
+
+// `
